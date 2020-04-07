193aa4ffdd78b4789af17f06210c0a9aa8021d08
Joao Aires <joaoaires98@gmail.com>
2020-04-07 12:40:59 +0100

    feito ate -n

--- mycat.c
+++ mycat.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <wchar.h>
-// ponto 1 feito
+#include <unistd.h>
+#include <getopt.h>
 
-void print (char const *argv[], int i)
+#define MAXSIZE 200
+//feito ate -n
+
+int print (char *const *argv, int i, int nFlag, int sum)
 {
 	char ch;
 
@@ -14,12 +17,30 @@ void print (char const *argv[], int i)
 		exit(1);
 	}
 
-	while((ch = fgetc(fp))!=EOF)
+	if(nFlag == 1)
 	{
-		printf("%c", ch);
+		char *str;
+		str = (char *) malloc(MAXSIZE);
+
+		while(fgets(str, MAXSIZE, fp) != NULL)
+		{
+			printf("%6d\t""%s", sum, str);
+			sum++;
+		}
+		sum--;
+
+		free(str);
+	}
+	else
+	{ 
+		while((ch = fgetc(fp))!=EOF)
+		{
+			printf("%c", ch);
+		}
 	}
 
 	fclose(fp);
+	return sum;
 }
 
 void print_stdin()
@@ -28,13 +49,12 @@ void print_stdin()
 	int i=0;
 	while((ch = fgetc(stdin)) != EOF)
 	{
-		if(i==0) printf("\n");
 		printf("%c", ch);
 		i++;
 	}
 }
 
-int file_exists(char const *argv[], int i)
+int file_exists(char *const *argv, int i)
 {
 	FILE *fp = fopen(argv[i], "r");
 	if(fp == NULL) return 0;
@@ -42,14 +62,37 @@ int file_exists(char const *argv[], int i)
 	return 1;
 } 
 
-int main(int argc, char const *argv[])
+int main(int argc, char *const *argv)
 {
-	int i=1;
+	int nFlag 	= 0;
+	int bFlag 	= 0;
+	int sFlag 	= 0;
+
+	int i = 1;
 	if(argc == 1)	
 	{
 		print_stdin();
 	}
 
+	int current_sum = 1;
+	int opt;
+	while((opt = getopt(argc, argv, ":nbs")) != -1)
+	{
+		switch(opt)
+		{
+			case 'n':
+				nFlag = 1;
+				break;
+			case 'b':
+				bFlag = 1;
+				break;
+			case 's':
+				sFlag = 1;
+				break;
+		}
+	}
+
+	i = optind;
 	while(i<argc)
 	{
 		if(argv[i][0] == '-')
@@ -59,7 +102,7 @@ int main(int argc, char const *argv[])
 
 		if(file_exists(argv,i))
 		{
-			print(argv, i);
+			current_sum += print(argv, i, nFlag, current_sum);
 		}
 		
 		i++;
